1a6eada84574fe79b6ea9aaa97c994a847fcdf4d
Ridwat <okunlolaridwanat@gmail.com>
2025-02-26 22:39:27 +0100

    added

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
       </head>
       <body className="">
-        <ClerkProvider frontendApi="pk_live_Y2xlcmsubXVzbGltZ3JvdXBvZnNjaG9vbHMuY29tJA">
+        <ClerkProvider frontendApi="pk_test_cHJvLWRpbmdvLTUxLmNsZXJrLmFjY291bnRzLmRldiQ">
           <DatabaseProvider> 
           <InactivityTimeout />
 
